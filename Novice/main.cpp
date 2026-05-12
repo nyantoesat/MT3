3e79b9e10db6950392d8bf052fedd7711e971b61
NyanToeSat <k025g1034@g.neec.ac.jp>
2026-05-12 10:59:57 +0900

    commit

--- Novice/main.cpp
+++ Novice/main.cpp
@@ -1,6 +1,89 @@
 #include <Novice.h>
+#include <cmath>
 
 const char kWindowTitle[] = "GC2B_03_ニャン_トー_セッ";
+
+
+struct Vector3 {
+	float x;
+	float y;
+	float z;
+};
+
+struct Matrix4x4 {
+	float m[4][4];
+};
+
+Matrix4x4 MakeRotateXMatrix(float radiam) {
+	Matrix4x4 rotateXMatrix = {};
+	rotateXMatrix.m[0][0] = 1.0f;
+	rotateXMatrix.m[1][1] = cosf(radiam);
+	rotateXMatrix.m[1][2] = sinf(radiam);
+	rotateXMatrix.m[2][1] = -sinf(radiam);
+	rotateXMatrix.m[2][2] = cosf(radiam);
+	rotateXMatrix.m[3][3] = 1.0f;
+	return rotateXMatrix;
+}
+
+Matrix4x4 MakeRotateYMatrix(float radiam) {
+	Matrix4x4 rotateYMatrix = {};
+	rotateYMatrix.m[0][0] = cosf(radiam);
+	rotateYMatrix.m[0][2] = -sinf(radiam);
+	rotateYMatrix.m[1][1] = 1.0f;
+	rotateYMatrix.m[2][0] = sinf(radiam);
+	rotateYMatrix.m[2][2] = cosf(radiam);
+	rotateYMatrix.m[3][3] = 1.0f;
+	return rotateYMatrix;
+}
+
+Matrix4x4 MakeRotateZMatrix(float radiam) {
+	Matrix4x4 rotateZMatrix = {};
+	rotateZMatrix.m[0][0] = cosf(radiam);
+	rotateZMatrix.m[0][1] = sinf(radiam);
+	rotateZMatrix.m[1][0] = -sinf(radiam);
+	rotateZMatrix.m[1][1] = cosf(radiam);
+	rotateZMatrix.m[2][2] = 1.0f;
+	rotateZMatrix.m[3][3] = 1.0f;
+	return rotateZMatrix;
+}
+
+Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2) {
+	Matrix4x4 result = {};
+	for (int row = 0; row < 4; ++row) {
+		for (int column = 0; column < 4; ++column) {
+			for (int k = 0; k < 4; ++k) {
+				result.m[row][column] += m1.m[row][k] * m2.m[k][column];
+			}
+		}
+	}
+	return result;
+}
+
+Matrix4x4 MakeRotateXYZMatrix(float xRadiam, float yRadiam, float zRadiam) {
+	Matrix4x4 x = MakeRotateXMatrix(xRadiam);
+	Matrix4x4 y = MakeRotateYMatrix(yRadiam);
+	Matrix4x4 z = MakeRotateZMatrix(zRadiam);
+	return Multiply(Multiply(x, y), z);
+}
+
+static const int kRowHeight = 20;
+static const int kColumnWidth = 60;
+
+void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label) {
+	Novice::ScreenPrintf(x, y, "%0.2f", vector.x);
+	Novice::ScreenPrintf(x + kColumnWidth, y, "%0.2f", vector.y);
+	Novice::ScreenPrintf(x + kColumnWidth * 2, y, "%0.2f", vector.z);
+	Novice::ScreenPrintf(x + kColumnWidth * 3, y, "%s", label);
+}
+
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label) {
+	Novice::ScreenPrintf(x, y, "%s", label);
+	for (int row = 0; row < 4; ++row) {
+		for (int column = 0; column < 4; ++column) {
+			Novice::ScreenPrintf(x + column * kColumnWidth, y + (row + 1) * kRowHeight, "%6.02f", matrix.m[row][column]);
+		}
+	}
+}
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
@@ -11,6 +94,9 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
+
+	Vector3 rotate = {0.4f, 1.43f, -0.8f};
+
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -24,6 +110,10 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		///
 		/// ↓更新処理ここから
 		///
+		Matrix4x4 rotateXMatrix = MakeRotateXMatrix(rotate.x);
+		Matrix4x4 rotateYMatrix = MakeRotateYMatrix(rotate.y);
+		Matrix4x4 rotateZMatrix = MakeRotateZMatrix(rotate.z);
+		Matrix4x4 rotateXYZMatrix = MakeRotateXYZMatrix(rotate.x, rotate.y, rotate.z);
 
 		///
 		/// ↑更新処理ここまで
@@ -33,6 +123,11 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		/// ↓描画処理ここから
 		///
 
+		MatrixScreenPrintf(0, 0, rotateXMatrix, "rotateXMatrix");	
+		MatrixScreenPrintf(0, kRowHeight *5 , rotateYMatrix, "rotateYMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 5 * 2, rotateZMatrix, "rotateZMatrix");	
+		MatrixScreenPrintf(0, kRowHeight * 5 * 3, rotateXYZMatrix, "rotateXYZMatrix");
+		
 		///
 		/// ↑描画処理ここまで
 		///
